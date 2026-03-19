c2b20b18178c21a8cc1c3f6cbed85e17988a7bb3
Matthew Messinger <mtmessinger@yahoo.com>
2026-03-19 13:01:59 -0400

    tweak

--- cxi_test_showallvals.py
+++ cxi_test_showallvals.py
@@ -6,7 +6,6 @@ fc = cxi(MB_ADDRESS, USB)
 fc.temperature_units='F'
 
 print(f"fan coil is on: {fc.is_on()}")
-print(f"room temperature: {fc.get_roomtemp()}{fc.temperature_units}")
 print(f"coil temperature: {fc.get_coiltemp()}{fc.temperature_units}")
 print(f"cool target: {fc.get_cool_target()}{fc.temperature_units}")
 print(f"heat target: {fc.get_heat_target()}{fc.temperature_units}")
